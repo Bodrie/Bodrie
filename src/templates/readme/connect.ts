6837e0b7fad10ebd1ce04a144b202865ae19942e
Dobromir Kirov <kirov0407@gmail.com>
2026-02-17 18:30:01 +0200

    chore: socials refinement

--- src/templates/readme/connect.ts
+++ src/templates/readme/connect.ts
@@ -9,7 +9,7 @@ const socialLinks: SocialLink[] = [
   {
     name: 'LinkedIn',
     url: 'https://www.linkedin.com/in/d-kirov/',
-    icon: 'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg',
+    icon: 'https://img.shields.io/badge/LinkedIn-%230077B5.svg?style=for-the-badge&logo=data%3Aimage%2Fsvg%2Bxml%3Bbase64%2CPHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0id2hpdGUiPjxwYXRoIGQ9Ik0xOSAwaC0xNGMtMi43NjEgMC01IDIuMjM5LTUgNXYxNGMwIDIuNzYxIDIuMjM5IDUgNSA1aDE0YzIuNzYyIDAgNS0yLjIzOSA1LTV2LTE0YzAtMi43NjEtMi4yMzgtNS01LTV6bS0xMSAxOWgtM3YtMTFoM3YxMXptLTEuNS0xMi4yNjhjLS45NjYgMC0xLjc1LS43OS0xLjc1LTEuNzY0cy43ODQtMS43NjQgMS43NS0xLjc2NCAxLjc1Ljc5IDEuNzUgMS43NjQtLjc4MyAxLjc2NC0xLjc1IDEuNzY0em0xMy41IDEyLjI2OGgtM3YtNS42MDRjMC0zLjM2OC00LTMuMTEzLTQgMHY1LjYwNGgtM3YtMTFoM3YxLjc2NWMxLjM5Ni0yLjU4NiA3LTIuNzc3IDcgMi40NzZ2Ni43NTl6Ii8%2BPC9zdmc%2B',
     height: 30,
   },
   {
@@ -27,7 +27,7 @@ const socialLinks: SocialLink[] = [
   {
     name: 'GitHub',
     url: 'https://github.com/Bodrie',
-    icon: 'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg',
+    icon: 'https://img.shields.io/badge/github-gray?style=for-the-badge&logo=github',
     height: 30,
   },
 ];
